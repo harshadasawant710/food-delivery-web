2e09e732b8c3c83052d568d51fa533632deee1f4
harshada sawant <harshadasawant710@gmail.com>
2026-06-20 19:02:42 +0530

    RES_OWNER authorization for restaurant,menuCategory and menuItem

--- src/routers/menuCategoryRoutes.ts
+++ src/routers/menuCategoryRoutes.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import MenuCategoryController  from '../controllers/MenuCategoryController';
 import validateMiddleware from '../middlewares/validateMiddleware';
 import MenuCategoryService from '../validators/menuCategoryValidator';
+import resOwnerMiddleware from '../middlewares/resOwnerMiddleware'
+import authMiddleware from '../middlewares/authMiddleware'
 
 const router = express.Router();
 
 const menuCategoryController = new MenuCategoryController();
 
-router.post('/create', validateMiddleware.validate(MenuCategoryService) , menuCategoryController.createMenuCategory);
+router.post('/create',authMiddleware, resOwnerMiddleware,validateMiddleware.validate(MenuCategoryService) , menuCategoryController.createMenuCategory);
 router.post('/create/bulk', menuCategoryController.createBulkMenuCategory)
 router.put('/:id', validateMiddleware.validate(MenuCategoryService) ,menuCategoryController.updateMenuCategory);
 router.delete('/delete/bulk', menuCategoryController.deleteBulkMenuCategory)
--- src/routers/menuItemRoutes.ts
+++ src/routers/menuItemRoutes.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import MenuItemController  from '../controllers/MenuItemController';
 import validateMiddleware from '../middlewares/validateMiddleware';
 import menuItemValidator from '../validators/menuItemValidator';
-
+import resOwnerMiddleware from '../middlewares/resOwnerMiddleware'
+import authMiddleware from '../middlewares/authMiddleware'
 const router = express.Router();
 
 const menuItemController = new MenuItemController();
 
-router.post('/create', validateMiddleware.validate(menuItemValidator) , menuItemController.createMenuItem);
+router.post('/create',authMiddleware,resOwnerMiddleware,validateMiddleware.validate(menuItemValidator) , menuItemController.createMenuItem);
 router.post('/create/bulk', menuItemController.createBulkMenuItem)
 router.put('/:id', validateMiddleware.validate(menuItemValidator) ,menuItemController.updateMenuItem);
 router.delete('/delete/bulk', menuItemController.deleteBulkMenuItem)
